b61c6994511e29ca05ac63bcf558ef80acc1d110
davidkdwyer <davidkdwyer@gmail.com>
2013-06-01 14:15:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MircTestBlog::Application.config.secret_token = '3984707425bd133f8f2ac5831118f1a6b036bd55cf4c5906a5846f26619b1c477c9337874962079abe44eb276d919d4e58ee47e07e2cc08c44d68383e7c56dc5'
